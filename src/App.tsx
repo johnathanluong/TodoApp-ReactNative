6c6fc1f085a57a0a4e2f96e268d0dba56222abcb
John Luong <johnathanluong7@gmail.com>
2024-02-22 17:12:02 -0500

    Set up environment + Make header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
     </View>
   );
 }
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+registerRootComponent(App);
